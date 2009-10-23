a9ca23af0a71059319d2e3f66305a80927607ce0
nfk <nicofonk@gmail.com>
2009-10-24 00:12:48 +0200

    test

---
+++ src/game/test/test.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf8 -*-
+# $Id: $
+'''
+Project : Tarot Web [https://launchpad.net/tarot]
+Author : nfk
+Date    : 24 oct. 2009
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
